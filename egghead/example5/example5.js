38f14a539b730e459953383650b41d427673723c
isortegah <isortegah@gmail.com>
2017-06-04 16:38:24 -0500

    Ejemplo 5

---
+++ egghead/example5/example5.js
@@ -0,0 +1,15 @@
+/**
+* Created by ISORTEGAH on 04/06/2017.
+**/
+
+let firstName = "Ivan";
+let lastName  = "Ortega"
+
+let person = { firstName , lastName }
+
+console.log(person)
+
+let mascot = "Raton"
+let team = { person , mascot }
+
+console.log( team );
