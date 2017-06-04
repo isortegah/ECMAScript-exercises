f1b3ef23803c1f38ec8d5131abc381543c8fb0ff
IORTREGA <iortega@nearbpo.com>
2017-06-03 19:20:30 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+## Compiladores   
+[Traceur](https://github.com/google/traceur-compiler)  
+[Babel](https://babeljs.io/)
---
+++ egghead/example1.js
@@ -0,0 +1,17 @@
+/**
+* Created by ISORTEGAH on 03/06/2017.
+**/
+var deliveryBoy = {
+    name : "Ivan Sebastian",
+
+    handlerMessage : function ( message , handler ) {
+        handler ( message );
+    },
+
+    receive : function () {
+
+        this.handlerMessage ( "Hello, " , message => console.log( message + this.name ))
+    }
+}
+
+deliveryBoy.receive();
