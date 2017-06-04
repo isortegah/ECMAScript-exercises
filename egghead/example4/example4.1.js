dde0ecf9443abf6559f012af9c10f30c4a4d9897
isortegah <isortegah@gmail.com>
2017-06-04 16:24:36 -0500

    Ejemplo 4

---
+++ egghead/example4/example4.1.js
@@ -0,0 +1,9 @@
+/**
+* Created by ISORTEGAH on 04/06/2017.
+**/
+if(true){
+    const foo = 'bar'
+}
+
+console.log('foo: ' + foo);
+
---
+++ egghead/example4/example4.compile.js
@@ -0,0 +1,7 @@
+/**
+* Created by ISORTEGAH on 04/06/2017.
+**/
+//var VALUE = 'hello world';
+const VALUE = 'hello world';
+VALUE = 'foo bar';
+console.log('value: ', VALUE);
